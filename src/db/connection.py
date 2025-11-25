851be02723e177587ae1f018f622b8c9e17baacb
kurama <paul.zenker.pz@gmail.com>
2025-11-25 15:10:20 +0100

    feat: added execute() and fetch() functions

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,7 +1,7 @@
 import functools
-from typing import Awaitable, Callable, Coroutine, Optional
+from typing import Awaitable, Callable, Coroutine, Optional, List, Any
 import asyncpg
-from asyncpg import Pool
+from asyncpg import Pool, Connection, Record
 
 from utils.singleton import SingletonMeta
 
@@ -11,10 +11,10 @@ def aquire(coroutine: Callable):
     an aquired connection as first parameter
     """
     # aquire pool from DatabaseConnection
-    pool = DatabaseConnection.get_instance().pool
-    
+    pool = Database.get_instance().pool
+
     @functools.wraps(coroutine)
-    async def wrapper(*coro_args, **coro_kwargs):
+    async def wrapper(self: "Database", *coro_args, _cxn: Connection, **coro_kwargs):
         async with pool.acquire() as connection:
             # aquired connection
             async with connection.transaction():
@@ -22,16 +22,27 @@ def aquire(coroutine: Callable):
                 # -> call coroutine inside the transaction
                 # and pass the connection as first arg
                 return await coroutine(
-                    connection, 
-                    *coro_args, 
+                    self,
+                    *coro_args,
+                    _cxn=connection,
                     **coro_kwargs
                 )
     
     return wrapper
 
+def copy_docs(copy_from_func: Callable):
+    """decorator factory to replace the function docs with the given ones"""
+    def decocator(func: Callable):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        wrapper.__doc__ = copy_from_func.__doc__
+        return wrapper
+    return decocator
 
-class DatabaseConnection(metaclass=SingletonMeta):
-    _instance: Optional["DatabaseConnection"] = None
+
+class Database(metaclass=SingletonMeta):
+    _instance: Optional["Database"] = None
     def __init__(self, dsn: str):
         self._pool: Optional[Pool] = None
         self._dsn: str = dsn
@@ -53,6 +64,47 @@ class DatabaseConnection(metaclass=SingletonMeta):
         return self._pool
 
     @classmethod
-    def get_instance(cls) -> "DatabaseConnection":
+    def get_instance(cls) -> "Database":
         assert cls._instance
         return cls._instance
+
+    @aquire
+    async def execute(self, query: str, *args: List[Any], _cxn: Connection) -> str:
+        """
+        Execute an SQL command (or commands).
+
+        This method can execute many SQL commands at once, when no arguments are provided.
+
+        Example:
+        ```
+        >>> await execute('''
+        ...     CREATE TABLE mytab (a int);
+        ...     INSERT INTO mytab (a) VALUES (100), (200), (300);
+        ... ''')
+        INSERT 0 3
+        ```
+
+        ```
+        >>> await execute('''
+        ...     INSERT INTO mytab (a) VALUES ($1), ($2)
+        ... ''', 10, 20)
+        INSERT 0 2
+        ```
+
+        args: Query arguments.
+        """
+        return await _cxn.execute(query, *args)
+
+    
+    @aquire
+    async def fetch(self, query: str, *args: List[Any], _cxn: Connection) -> List[Record]:
+        """use when making selections.
+
+        Returns:
+        --------
+        List[Record]:
+            the records from the selection/return
+        """
+        return await _cxn.fetch(query, *args)
+
+
