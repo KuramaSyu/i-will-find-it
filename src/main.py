0041d0d780b23431ec65dec06de611c0824b3339
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-11-28 19:27:54 +0100

    chore: cleaned up imports

--- src/main.py
+++ src/main.py
@@ -1,29 +1,25 @@
 import asyncio
 import grpc
-from grpc_mod.proto.note_pb2_grpc import add_NoteServiceServicer_to_server
-from grpc_mod.service import GRPCNoteService
+from grpc_mod import add_NoteServiceServicer_to_server, GRPCNoteService
 from db.repos import NoteRepoABC, NotePostgreRepo
+from db import Database
 
 
 async def serve():
-    # 1. Initialize your repository (async DB layer)
-    repo: NoteRepoABC = NotePostgreRepo()  # replace with your actual async repo implementation
+    db = Database(dsn="postgres://postgres:postgres@localhost:5433/db?sslmode=disable")
+    await db.init_db()
+    repo: NoteRepoABC = NotePostgreRepo(db=db)
+    note_service = GRPCNoteService(repo=repo)
 
-    # 2. Create your gRPC server
     server = grpc.aio.server()
+    add_NoteServiceServicer_to_server(note_service, server)
 
-    # 3. Register your service
-    add_NoteServiceServicer_to_server(GRPCNoteService(repo), server)
-
-    # 4. Listen on a port
     listen_addr = "[::]:50051"
     server.add_insecure_port(listen_addr)
     print(f"gRPC server listening on {listen_addr}")
 
-    # 5. Start the server
+    # Start the server
     await server.start()
-
-    # 6. Wait forever (or until cancelled)
     await server.wait_for_termination()
 
 
