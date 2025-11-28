741d98b49960483e420f6f7289688079f27dab64
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-11-28 16:32:19 +0100

    feat: implemented get_note

--- src/grpc/service.py
+++ src/grpc/service.py
@@ -1,7 +1,8 @@
 from grpc.aio import ServicerContext
 
-from generated import NoteService, NoteServiceServicer, NoteServiceStub, GetNoteRequest, Note, PostNoteRequest
 from db.repos import NoteRepoABC
+from db.entities import NoteEntity
+from grpc.generated import PostNoteRequest, GetNoteRequest, NoteServiceServicer, Note
 
 
 class GRPCNoteService(NoteServiceServicer):
@@ -13,7 +14,15 @@ class GRPCNoteService(NoteServiceServicer):
         self.repo = repo
 
     async def get_note(self, request: GetNoteRequest, ctx: ServicerContext) -> Note:
-        ...
+        await self.repo.select(note=NoteEntity(
+            note_id=request.id,
+            author_id=None,
+            content=None,
+            embeddings=None,
+            permissions=None,
+            title=None,
+            updated_at=None
+        ))
 
     async def post_note(self, request: PostNoteRequest, ctx: ServicerContext) -> Note:
         ...
