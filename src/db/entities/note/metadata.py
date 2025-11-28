0e0aa9a67d8f3e819e3a384c3f8c1264af11d1bd
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-11-28 13:34:37 +0100

    api: added some attrs

--- src/db/entities/note/metadata.py
+++ src/db/entities/note/metadata.py
@@ -1,8 +1,17 @@
 from dataclasses import dataclass
+from typing import List
+from datetime import datetime
+
+from .embedding import NoteEmbeddingEntity
+from .permission import NotePermissionEntity
 
 @dataclass
-class NoteMetadataEntity:
+class NoteEntity:
     """Represents one record of note.metadata"""
     note_id: int
     title: str
+    updated_at: datetime
     author_id: int
+    content: str
+    embeddings: List[NoteEmbeddingEntity]
+    permissions: List[NotePermissionEntity]
