c6a054b4f387d5ed6123c0053d09e792c7b79068
kurama <paul.zenker.pz@gmail.com>
2025-12-02 09:53:07 +0100

    chore: added user proto imports

--- src/grpc_mod/__init__.py
+++ src/grpc_mod/__init__.py
@@ -1,3 +1,5 @@
 from .proto.note_pb2_grpc import add_NoteServiceServicer_to_server, NoteService, NoteServiceServicer, NoteServiceStub
 from .proto.note_pb2 import GetNoteRequest, Note, NotePermission, PostNoteRequest, NoteEmbedding
+from .proto.user_pb2_grpc import add_UserServiceServicer_to_server, UserService, UserServiceServicer, UserServiceStub
+from .proto.user_pb2 import User, GetUserRequest, AlterUserRequest, DeleteUserRequest, DeleteUserResponse
 from .service import GRPCNoteService
