26c96fb3888287beca7b41ecf4da8ab098f3f022
kurama <paul.zenker.pz@gmail.com>
2025-12-05 16:28:18 +0100

    api: note_id and role_id can be UNDEFINED

--- src/db/entities/note/permission.py
+++ src/db/entities/note/permission.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
+from api.undefined import UndefinedOr
+
 @dataclass
 class NotePermissionEntity:
     """Represents one record of note.metadata"""
-    note_id: int
-    role_id: int
+    note_id: UndefinedOr[int]
+    role_id: UndefinedOr[int]
