987938b06b5a5c68f371f4aa8db1055e5a7c1eb3
kurama <paul.zenker.pz@gmail.com>
2025-12-08 12:46:58 +0100

    chore: updated imports

--- src/grpc_mod/__init__.py
+++ src/grpc_mod/__init__.py
@@ -1,5 +1,9 @@
 from .proto.note_pb2_grpc import add_NoteServiceServicer_to_server, NoteService, NoteServiceServicer, NoteServiceStub
-from .proto.note_pb2 import GetNoteRequest, Note, NotePermission, PostNoteRequest, NoteEmbedding, GetSearchNoteRequest, MinimalNote 
+from .proto.note_pb2 import (
+    GetNoteRequest, Note, NotePermission, 
+    PostNoteRequest, NoteEmbedding, GetSearchNotesRequest,
+    MinimalNote
+)
 from .proto.user_pb2_grpc import add_UserServiceServicer_to_server, UserService, UserServiceServicer, UserServiceStub
 from .proto.user_pb2 import User, GetUserRequest, AlterUserRequest, DeleteUserRequest, DeleteUserResponse, PostUserRequest
 from .service import *
